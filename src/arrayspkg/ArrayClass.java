fa4e0f3d22ddc6bf1cc9bd40f353859335e1fd21
Ahsan Naushad <ahsannaushadkarim@gmail.com>
2018-07-25 20:38:39 +0500

    Pushing all my preparations.

---
+++ src/arrayspkg/ArrayClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayspkg;
+
+import java.util.*;
+
+/**
+ *
+ * @author ahsan
+ */
+public class ArrayClass
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        ArrayList MyArrayList = new ArrayList();
+        MyArrayList.add("Ahsan");
+        MyArrayList.add("Arbaz");
+        MyArrayList.add("Sarah");
+        System.out.println("My ArrayList is "+MyArrayList);
+        System.out.println("The even numbers in ArrayList are: ");
+        System.out.println("The odd numbers in ArrayList are: ");
+    }   
+}
